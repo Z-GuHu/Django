7d64dc71ecec68e1bebb037bd710e7e48a3328c0
unknown <zsq215@qq.com>
2023-09-09 16:47:15 +0800

    视图和路由匹配

---
+++ bookmanager/book/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from book.views import index
+
+urlpatterns = [
+
+    #添加一条路由匹配
+    #格式  path('路由’，视图函数名）
+    path('index/abc', index),
+    path('abc/', index),
+]
